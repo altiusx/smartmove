019638ea7c850589c36719fa49fb6ba5c6558804
Christopher Choong <69862812+altiusx@users.noreply.github.com>
2021-06-06 22:25:39 +0800

    fixes

--- lib/services/constant_strings.dart
+++ lib/services/constant_strings.dart
@@ -1,6 +1,6 @@
 const String isLogin = 'isLogin';
 const String isLoginType = 'isLoginType';
-const String apiKey = "NmG2lFn5QhK9+yoir1y6mQ=="; //LTA Datamall api key
+const String apiKey = "..."; //LTA Datamall api key
 
 
 const String taskChannelId = "10001"; //must be unique
